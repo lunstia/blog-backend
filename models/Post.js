22e5a35761dd13b56bb7043b07894e45b822d954
lunstia <136163666+lunstia@users.noreply.github.com>
2023-08-13 19:46:53 -0400

    Add featured to model

--- models/Post.js
+++ models/Post.js
@@ -5,6 +5,7 @@ const Post = new Schema({
     title: {type: String, required},
     post: {type: String, required},
     published: {type: Boolean, default: false},
+    featured: {type: Boolean, default: false}, // This is a manual selection for now, might add a view system and feature base off that.
     datePublished: {type: Date, default: Date.now()}
 })
 
