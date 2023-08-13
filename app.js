4a4d702524f42a0c28dba219b696ad443827725a
lunstia <136163666+lunstia@users.noreply.github.com>
2023-08-13 19:46:40 -0400

    Add mongoose boilerplate

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
+
+mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
 
 const app = express();
 
