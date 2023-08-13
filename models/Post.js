88860fef6263d0adae3c8894f186ee8cd330860c
lunstia <136163666+lunstia@users.noreply.github.com>
2023-08-13 16:41:55 -0400

    Edit post model

--- models/Post.js
+++ models/Post.js
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from 'mongoose';
+import { Schema, model } from 'mongoose';
 
 const Post = new Schema({
     author: {type: Schema.Types.ObjectId, ref: 'User', required},
@@ -8,4 +8,4 @@ const Post = new Schema({
     datePublished: {type: Date, default: Date.now()}
 })
 
-export default mongoose.model('Post', Post);
+export default model('Post', Post);
