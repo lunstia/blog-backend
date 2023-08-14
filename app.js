fcd8a3bb88001e3e05f5205373c28dc52052c883
lunstia <136163666+lunstia@users.noreply.github.com>
2023-08-14 14:31:09 -0400

    Edit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import index from './routes/index.js';
+import posts from './routes/posts.js';
+
 mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
@@ -10,10 +13,7 @@ const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: "Hello, World!"
-    });
-})
+app.use('/', index);
+app.use('/posts', posts);
 
 app.listen(process.env.PORT, () => console.log("Listening on " + process.env.PORT))
