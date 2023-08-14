cbadee853c7fa64f9ec638c88f367bcf8a23b4af
lunstia <136163666+lunstia@users.noreply.github.com>
2023-08-14 14:57:55 -0400

    Add pseudocode(ish)

--- routes/posts.js
+++ routes/posts.js
@@ -7,4 +7,40 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+    // get post and comments
+});
+
+
+router.post('/', (req, res) => {
+    // Make sure to verify jwt token
+    // Posts a post
+    res.json({
+        message: "Not implemented yet"
+    });
+});
+
+router.post('/:id', (req, res) => {
+    // post a comment
+})
+
+router.update('/:id', (req, res) => {
+    // update a post
+})
+
+router.update('/:id/:comment_id', (req, res) => {
+    // updates a comment
+})
+
+router.delete('/:id', (req, res) => {
+    // deletes a post
+});
+
+router.delete('/:id/:comment_id', (req, res) => {
+    // deletes a comment
+});
+
+
+
+
 export default router;
