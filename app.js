876c990646eacad17da4ddaedafd5b71ae0c1a91
lunstia <136163666+lunstia@users.noreply.github.com>
2023-08-16 22:10:13 -0400

    Remove unused code

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
@@ -12,8 +13,12 @@ db.on("error", console.error.bind(console, "mongo connection error"));
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.use('/', index);
 app.use('/posts', posts);
+
+
 
 app.listen(process.env.PORT, () => console.log("Listening on " + process.env.PORT))
