c05399416bb311be9db7cb77d23138328e1f0df8
lunstia <136163666+lunstia@users.noreply.github.com>
2023-08-16 22:08:03 -0400

    Add util directory and jwt verification

---
+++ utils/verifyToken.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+
+export default async function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['auth'];
+
+    if (bearerHeader === undefined) {
+        res.status(403).json({message: "User is unauthorized"});
+        return
+    }
+
+    try {
+        const bearerToken = bearerHeader.split(' ')[1];
+        const authData = await jwt.verify(bearerToken, process.env.SECRET_KEY);
+        req.user = authData.user;
+    } catch(err) {
+        res.status(403).json({message: "User is unauthorized"});;
+        return
+    }
+
+    next();
+}
