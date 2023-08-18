1fbd0f218d63eed9d522ae728304c94551aa9bed
lunstia <136163666+lunstia@users.noreply.github.com>
2023-08-18 11:42:15 -0400

    Add postController to respective crud methods

--- routes/posts.js
+++ routes/posts.js
@@ -1,36 +1,32 @@
 import { Router } from 'express';
-import verifyToken from '../utils/verifyToken.js';
 import postController from '../controllers/posts.js';
+import Post from '../models/Post.js';
+import Comment from '../models/Comment.js';
+import {isValidObjectId} from 'mongoose';
+
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        message: "Get Posts!"
-    });
-});
+router.get('/', postController.get_Posts);
 
-router.get('/:id', (req, res) => {
-    // get post and comments
-});
-
-
-router.post('/', postController.Posts_post);
-
-router.post('/:id', (req, res) => {
-    // post a comment
-})
+router.post('/', postController.post_Posts);
 
 router.put('/:id', (req, res) => {
     // update a post
 })
 
-router.put('/:id/:comment_id', (req, res) => {
-    // updates a comment
-})
-
 router.delete('/:id', (req, res) => {
     // deletes a post
 });
+
+router.get('/:id', postController.get_Posts_Comments);
+
+router.post('/:id', (req, res) => {
+    // post a comment
+})
+
+router.put('/:id/:comment_id', (req, res) => {
+    // updates a comment
+})
 
 router.delete('/:id/:comment_id', (req, res) => {
     // deletes a comment
