92cdad8761de002148e6b8c3c8d7bd862f6436d7
lunstia <136163666+lunstia@users.noreply.github.com>
2023-08-18 11:41:00 -0400

    Add get for posts and get for post + comments

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,10 +4,11 @@ import Post from '../models/Post.js';
 import Comment from '../models/Comment.js';
 import verifyToken from '../utils/verifyToken.js';
 import {body, validationResult} from 'express-validator';
+import {isValidObjectId} from 'mongoose';
 
 const index = {};
 
-index.Posts_post = [
+index.post_Posts = [
     verifyToken, 
     (req, res) => {
         if (!req.user.isAdmin) {
@@ -19,5 +20,32 @@ index.Posts_post = [
         });
     }
 ]
+
+index.get_Posts = asyncHandler(async (req, res) => {
+    const posts = await Post.find({published: true}).sort({datePublished: -1}).limit(25);
+
+    res.json({
+        posts
+    });
+})
+
+index.get_Posts_Comments = asyncHandler(async (req, res) => {
+    if (!isValidObjectId(req.params.id)) {
+        res.sendStatus(404);
+        return;
+    }
+
+    const [post, comments] = await Promise.all([
+        await Post.findById(req.params.id),
+        await Comment.find({post: req.params.id}).sort({date: -1}).limit(25)
+    ])
+
+    if (post === null) {
+        res.sendStatus(404);
+        return;
+    }
+
+    res.json({post, comments}); // Comments can return as an empty array cause it's fine for it to be empty.
+});
 
 export default index
