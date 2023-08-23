5ded49b1f8f03e22ed1f78d2670f5844e755c73d
lunstia <136163666+lunstia@users.noreply.github.com>
2023-08-22 21:33:46 -0400

    Add update to posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -80,9 +80,75 @@ index.get_Posts_Comments = asyncHandler(async (req, res, next) => {
     res.json({post, comments}); // Comments can return as an empty array cause it's fine for it to be empty.
 });
 
-index.update_Post = asyncHandler(async (req, res) => {
-    res.json({message: "Not implemented yet"});
-});
+index.update_Post = [
+    verifyToken,
+    verifyAdmin,
+    body("title")
+        .trim()
+        .exists()
+        .escape(),
+    body("post")
+        .trim()
+        .exists()
+        .escape(),
+    body("publish")
+        .isBoolean()
+        .optional()
+        .escape(),
+    body("feature")
+        .isBoolean()
+        .optional()
+        .escape(),
+    asyncHandler(async (req, res) => {
+        if (!isValidObjectId(req.params.id)) {
+            res.sendStatus(404);
+            return;
+        }
+
+        const result = validationResult(req)
+        if (!result.isEmpty) {
+            res.send(400).json({errors: result.errors});
+            return;
+        }
+        
+        const post = Post.findById(req.params.id);
+
+        if (post === null) {
+            res.send(404).json({error: "Post was not found"});
+            return
+        }
+        
+        if (post.author !== req.user._id) {
+            res.send(403).json({error: "Only the author can update their own posts"});
+            return
+        }
+
+        let newPost;
+
+        if (post.published === false && req.body.publish === true) {
+            newPost = new Post({
+                _id: req.params.id,
+                title: req.body.title,
+                post: req.body.post,
+                published: req.body.publish,
+                featured: req.body.feature,
+                datePublished: Date.now(),
+            });
+        } else {
+            newPost = new Post({
+                _id: req.params.id,
+                title: req.body.title,
+                post: req.body.post,
+                published: req.body.publish,
+                featured: req.body.feature,
+                datePublished: post.datePublished,
+            });
+        }
+
+        await Post.updateOne(post, newPost);
+        res.json({postId: post._id});
+    })
+]
 
 index.delete_Post = asyncHandler(async (req, res) => {
     res.json({message: "Not implemented yet"});
