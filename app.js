932e28650e7707261f60289e649981873445700b
lunstia <136163666+lunstia@users.noreply.github.com>
2023-08-26 21:16:19 -0400

    Revert 8eb23a9884a3e636bb6fcd0fbf4d2c434facd395
    This reverts commit 8eb23a9884a3e636bb6fcd0fbf4d2c434facd395.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 
 import index from './routes/index.js';
 import posts from './routes/posts.js';
+import admin from './routes/admin.js';
 
 mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
@@ -17,5 +18,6 @@ app.use(express.urlencoded({extended: false}));
 
 app.use('/', index);
 app.use('/posts', posts);
+app.use('/admin', admin);
 
 app.listen(process.env.PORT, () => console.log("Listening on " + process.env.PORT))
---
+++ routes/admin.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import admin from '../controllers/admin.js';
+const router = Router();
+
+
+
+export default router;
