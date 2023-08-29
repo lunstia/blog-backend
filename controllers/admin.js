eb7c4509e619ca04e812c5d7bdd8b97045aec48c
lunstia <136163666+lunstia@users.noreply.github.com>
2023-08-28 22:04:14 -0400

    Add get posts in admin route

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,25 @@
 import asyncHandler from 'express-async-handler';
-import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 import Post from '../models/Post.js';
 import Comment from '../models/Comment.js';
+import verifyToken from '../utils/verifyToken.js';
+import verifyAdmin from '../utils/verifyAdmin.js';
 import {body, validationResult} from 'express-validator';
+import {isValidObjectId} from 'mongoose';
 
-const admin = {}
+const admin = {} // This mainly handles getting data that regular users shouldnt access and that's why posts route will also have some admin duties in there.
+
+admin.get_Posts = [
+    verifyToken,
+    verifyAdmin,
+    asyncHandler(async (req, res) => {
+        const posts = await Post.find({}, "title author datePublished featured").sort({datePublished: -1}).limit(25);
+
+        res.json({
+            posts
+        });
+    })
+]
 
 
 export default admin;
